4364f27be3ab2f2504df810a79789d6ef292d65d
Luis <moondad2022@gmail.com>
2024-12-25 00:17:34 -0800

    added a nav page

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,22 @@
+function Nav() {
+  return (
+    <nav id="wp_nav">
+      <ul id="wp_nav_list">
+        <li>
+          <a className="wp_nav_item">Who I Am</a>
+        </li>
+        <li>
+          <a className="wp_nav_item">My Skills</a>
+        </li>
+        <li>
+          <a className="wp_nav_item">Why Me?</a>
+        </li>
+        <li>
+          <a className="wp_nav_item">My Stuff</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
